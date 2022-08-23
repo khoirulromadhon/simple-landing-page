1fc68d046764c7627fcd8dc9b0b221c947f30a4b
khoirulromadhon <khoirulromadhon1112@gmail.com>
2022-08-23 14:41:53 +0700

    first commit

---
+++ resources/views/layouts/create.blade.php
@@ -0,0 +1,7 @@
+@extends('app.app')
+
+@section('content')
+<div>
+    <h1>Halaman Untuk CRUD</h1>
+</div>
+@endsection
---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,7 @@
+@extends('app.app')
+
+@section('content')
+<div class="jumbotron text-center">
+    <h1>Halaman Home</h1>
+</div>
+@endsection
